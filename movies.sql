047482c5d954e18579a440dc042fd47ca9faa0f1
Ryan Benney <99368582+acse-rgb123@users.noreply.github.com>
2024-10-22 13:47:31 +0100

    Add files via upload

--- movies.sql
+++ movies.sql
@@ -1,19 +1,46 @@
-use movielens;
-
--- I HAVENT DONE THIS NICELY YET SO I WILL POST AFTER ITS NICE
+USE movielens;
 
 -- List the titles and release dates of movies released between 1983-1993 in reverse chronological order.
-select title, release_date from movies where release_date > '1982-12-31' and release_date < '1994-01-01' order by release_date desc;
+SELECT title, release_date 
+FROM movies 
+WHERE release_date > '1982-12-31' 
+  AND release_date < '1994-01-01' 
+ORDER BY release_date DESC;
 
 -- Without using LIMIT, list the titles of the movies with the lowest average rating.
-select title from movies left join ratings on movies.id = ratings.id where ratings.rating = 1;
+SELECT title 
+FROM movies 
+LEFT JOIN ratings ON movies.id = ratings.movie_id 
+WHERE ratings.rating = 1;
 
 -- List the unique records for Sci-Fi movies where male 24-year-old students have given 5-star ratings.
-select title from movies inner join genres_movies on genres_movies.movie_id = movies.id inner join genres on genres.id = genres_movies.genre_id inner join ratings on ratings.movie_id = movies.id inner join users on users.id = ratings.user_id
-where users.age > 24 and ratings.rating = 5;
+SELECT DISTINCT title 
+FROM movies 
+INNER JOIN genres_movies ON genres_movies.movie_id = movies.id 
+INNER JOIN genres ON genres.id = genres_movies.genre_id 
+INNER JOIN ratings ON ratings.movie_id = movies.id 
+INNER JOIN users ON users.id = ratings.user_id
+WHERE genres.name = 'Sci-Fi' 
+  AND users.age = 24 
+  AND users.gender = 'M' 
+  AND users.occupation_id = (SELECT id FROM occupations WHERE name = 'Student') 
+  AND ratings.rating = 5;
 
 -- List the unique titles of each of the movies released on the most popular release day.
-select title from movies where release_date = (select release_date from movies group by release_date order by count(*) desc limit 1);
+SELECT title 
+FROM movies 
+WHERE release_date = (
+    SELECT release_date 
+    FROM movies 
+    GROUP BY release_date 
+    ORDER BY COUNT(*) DESC 
+    LIMIT 1
+);
 
 -- Find the total number of movies in each genre; list the results in ascending numeric order.
-select genres.name, count(movies.id) as total_movies from movies inner join genres_movies on genres_movies.movie_id = movies.id  inner join genres on genres.id = genres_movies.genre_id  group by genres.name order by total_movies asc;
+SELECT genres.name, COUNT(movies.id) AS total_movies 
+FROM movies 
+INNER JOIN genres_movies ON genres_movies.movie_id = movies.id  
+INNER JOIN genres ON genres.id = genres_movies.genre_id  
+GROUP BY genres.name 
+ORDER BY total_movies ASC;
